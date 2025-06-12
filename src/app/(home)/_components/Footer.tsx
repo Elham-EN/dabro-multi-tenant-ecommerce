eb5dc4dce0f7be18ce196ebc43ad2f18a6c66994
Elham-EN <ilhamrezaie@gmail.com>
2025-06-12 23:45:00 +1000

    added footer to the layout & fix component ui style issue

---
+++ src/app/(home)/_components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-between p-6 font-medium border-t">
+      <div className="flex items-center gap-2">
+        <p>Dabro, Inc.</p>
+      </div>
+    </footer>
+  );
+}
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./_components/Navbar";
+import Footer from "./_components/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,7 +10,8 @@ function Layout({ children }: LayoutProps): React.ReactElement {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <div className="flex-1 bg-[#f4f4f0]">{children}</div>
+      <Footer />
     </div>
   );
 }
